0bcee88b92dca5dce53accc1973b96287e77d649
yuma140902 <yuma140902@gmail.com>
2020-02-25 18:56:09 +0900

    SettingFormの文字サイズを大きくした

--- ShutdownCountdown/SettingForm.Designer.cs
+++ ShutdownCountdown/SettingForm.Designer.cs
@@ -37,18 +37,20 @@
 			// 
 			this.dateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.dateTimePicker.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-			this.dateTimePicker.Location = new System.Drawing.Point(12, 24);
+			this.dateTimePicker.Location = new System.Drawing.Point(12, 26);
 			this.dateTimePicker.Name = "dateTimePicker";
-			this.dateTimePicker.Size = new System.Drawing.Size(359, 19);
+			this.dateTimePicker.Size = new System.Drawing.Size(359, 34);
 			this.dateTimePicker.TabIndex = 0;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(143, 12);
+			this.label1.Size = new System.Drawing.Size(168, 14);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "シャットダウンする時間を指定:";
 			// 
@@ -56,7 +58,7 @@
 			// 
 			this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelBtn.Location = new System.Drawing.Point(185, 65);
+			this.cancelBtn.Location = new System.Drawing.Point(185, 68);
 			this.cancelBtn.Name = "cancelBtn";
 			this.cancelBtn.Size = new System.Drawing.Size(105, 23);
 			this.cancelBtn.TabIndex = 2;
@@ -67,7 +69,7 @@
 			// okBtn
 			// 
 			this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.okBtn.Location = new System.Drawing.Point(296, 65);
+			this.okBtn.Location = new System.Drawing.Point(296, 68);
 			this.okBtn.Name = "okBtn";
 			this.okBtn.Size = new System.Drawing.Size(75, 23);
 			this.okBtn.TabIndex = 3;
@@ -81,7 +83,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cancelBtn;
-			this.ClientSize = new System.Drawing.Size(383, 100);
+			this.ClientSize = new System.Drawing.Size(383, 103);
 			this.Controls.Add(this.okBtn);
 			this.Controls.Add(this.cancelBtn);
 			this.Controls.Add(this.label1);
